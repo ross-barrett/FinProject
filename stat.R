3b5200c516ec857bd8b61a27d806b76a12475620
ross-barrett <rossabarrett@gmail.com>
2013-11-13 00:30:19 -0600

    made changes

--- stat.R
+++ stat.R
@@ -1,1 +1,13 @@
-stat
+#import data
+tickers = scan("tickers.csv",what="list",sep=",")
+dates = scan("dates.csv",what="list",sep=",")
+earningsDates = scan("earningsDates.csv",what = "list", sep = ",")
+open = matrix(scan("openValues.csv",what=double(),sep=","),ncol=length(tickers),nrow=length(dates),byrow=T)
+close = matrix(scan("closeValues.csv",what=double(),sep=","),ncol=length(tickers),nrow=length(dates),byrow=T)
+low = matrix(scan("lowValues.csv",what=double(),sep=","),ncol=length(tickers),nrow=length(dates),byrow=T)
+high = matrix(scan("highValues.csv",what=double(),sep=","),ncol=length(tickers),nrow=length(dates),byrow=T)
+volume = matrix(scan("volumeValues.csv",what=double(),sep=","),ncol=length(tickers),nrow=length(dates),byrow=T)
+
+
+#clean the data by removing all incomplete columns
+
